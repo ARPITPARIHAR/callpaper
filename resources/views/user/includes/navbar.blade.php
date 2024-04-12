406107e198d61cf788e9fb74d507b53261dc82e1
Arpit Parihar <parihararpiy@gmail.com>
2024-04-12 14:14:10 +0530

    initial commit

---
+++ resources/views/user/includes/navbar.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags --> 
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Marshmallow Bootstrap Admin Dashboard Template</title>
+		
+	</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<div id="mobile-menu-overlay"></div>
+		<nav class="navbar navbar-expand-lg fixed-top">
+			<div class="container">
+				<a class="navbar-brand" href="#"><img src="images/logo.svg" alt="Marshmallow"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"><i class="mdi mdi-menu"> </i></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+					<div class="d-lg-none d-flex justify-content-between px-4 py-3 align-items-center">
+						<img src="images/logo-dark.svg" class="logo-mobile-menu" alt="logo">
+						<a href="javascript:;" class="close-menu"><i class="mdi mdi-close"></i></a>
+					</div>
+					<ul class="navbar-nav ml-auto align-items-center">
+						<li class="nav-item active">
+							<a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#services">Services</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#about">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#projects">Projects</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#testimonial">Testimonial</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#plans">Plans</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link btn btn-success" href="#contact">174-394-9560</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="page-body-wrapper">
+			<section id="home" class="home">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="main-banner">
+								<div class="d-sm-flex justify-content-between">
+									<div data-aos="zoom-in-up">
+										<div class="banner-title">
+												<h3 class="font-weight-medium">We Help Power
+												Millions Of Businesses 
+												in 100+ Countries
+											</h3>
+										</div>
+										<p class="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+
+											<br>
+											Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+										</p>
+										<a href="#" class="btn btn-secondary mt-3">Learn more</a>
+									</div>
+									<div class="mt-5 mt-lg-0">
+										<img src="images/group.png" alt="marsmello" class="img-fluid" data-aos="zoom-in-up">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+            <style>
+            .nav-link {
+                font-size: 18px !important; /* Font size ko 28px set kiya gaya hai */
+            }
+            </style>
