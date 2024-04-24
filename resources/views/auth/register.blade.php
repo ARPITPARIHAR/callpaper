05249bff733a2b158d09d2c29d1ad2743de2898e
Arpit Parihar <parihararpiy@gmail.com>
2024-04-24 11:40:16 +0530

    initial commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -35,77 +35,67 @@
     </style>
 
 <header class="navbar pcoded-header navbar-expand-lg navbar-light header-dark">
-		
+	<div class="m-header">
+		<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
+		<a href="#!" class="b-brand">
+			<!-- ========   change your logo hear   ============ -->
 			
-				<div class="m-header">
-					<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
-					<a href="#!" class="b-brand">
-						<!-- ========   change your logo hear   ============ -->
-						
-					</a>
-					<a href="#!" class="mob-toggler">
-						<i class="feather icon-more-vertical"></i>
-					</a>
+		</a>
+		<a href="#!" class="mob-toggler">
+			<i class="feather icon-more-vertical"></i>
+		</a>
+	</div>
+	<div class="collapse navbar-collapse">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a href="#!" class="pop-search"><i class="feather icon-search"></i></a>
+				<div class="search-bar">
+					<input type="text" class="form-control border-0 shadow-none" placeholder="Search hear">
+					<button type="button" class="close" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
 				</div>
-				<div class="collapse navbar-collapse">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item">
-							<a href="#!" class="pop-search"><i class="feather icon-search"></i></a>
-							<div class="search-bar">
-								<input type="text" class="form-control border-0 shadow-none" placeholder="Search hear">
-								<button type="button" class="close" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-						</li>
-						<li class="nav-item">
-						
-								
-								<div class="dropdown-menu profile-notification ">
-									<ul class="pro-body">
-										<li><a href="/logout" class="dropdown-item"><i class="fas fa-circle"></i> Logout</a></li>
-										{{-- <li><a href="email_inbox.html" class="dropdown-item"><i class="fas fa-circle"></i> My Messages</a></li> --}}
-										<li><a href="auth-signin.html" class="dropdown-item"><i class="fas fa-circle"></i> Lock Screen</a></li>
-									</ul>
-								</div>
-							</div>
-						</li>
-						<li class="nav-item">
-							
-							</div>
-						</li>
-					</ul>
-					<ul class="navbar-nav ml-auto">
-					
-						<li>
-							<div class="dropdown drp-user">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-top: 20px;font-size: 44px;">
-                                    <i class="feather icon-user"></i>
-                                </a>
-                                
-								<div class="dropdown-menu dropdown-menu-right profile-notification">
-									<div class="pro-head">
-										{{-- <img src="" class="" alt="User-Profile-Image"> --}}
-										<span>PublishingHelp</span>
-										<a href="auth-signin.html" class="dud-logout" title="Logout">
-											<i class="feather icon-log-out"></i>
-										</a>
-									</div>
-									<ul class="pro-body">
-										<li><a href="{{ route('logout') }}" class="dropdown-item"><i class="feather icon-log-out"></i>
-											Logout</a></li>
-
-										<li><a href="email_inbox.html" class="dropdown-item"><i class="feather icon-mail"></i> My Messages</a></li>
-										<li><a href="auth-signin.html" class="dropdown-item"><i class="feather icon-lock"></i> Lock Screen</a></li>
-									</ul>
-								</div>
-							</div>
-						</li>
-					</ul>
+			</li>
+			<li class="nav-item">
+			
+			
 				</div>
+			</li>
+		</ul>
+		<ul class="navbar-nav ml-auto">
+			<li>
+				<div class="dropdown">
 				
-			
-	</header>
+					</a>
+					
+			</li>
+			<li>
+				<div class="dropdown drp-user">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-top: 20px;font-size: 44px;">
+						<i class="feather icon-user"></i>
+					</a>
+					
+					<div class="dropdown-menu dropdown-menu-right profile-notification">
+						<div class="pro-head">
+							{{-- <img src="" class="" alt="User-Profile-Image"> --}}
+							<span>PublishingHelp</span>
+							<a href="auth-signin.html" class="dud-logout" title="Logout">
+								<i class="feather icon-log-out"></i>
+							</a>
+						</div>
+						<ul class="pro-body">
+							<li><a href="{{ route('logout') }}" class="dropdown-item"><i class="feather icon-log-out"></i>
+								Logout</a></li>
+
+							
+						</ul>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+		
+</header>
 		<nav class="pcoded-navbar  ">
 		<div class="navbar-wrapper  ">
 			<div class="navbar-content scroll-div " >
--- resources/views/forms/index.blade.php
+++ resources/views/forms/index.blade.php
@@ -43,104 +43,76 @@
                margin-top: 2%;
         }
         
-        /* Add this CSS to adjust the height dynamically based on screen size */
+     
 
     }
     </style>
 </head>
 <header class="navbar pcoded-header navbar-expand-lg navbar-light header-dark">
-		
-			
-				<div class="m-header">
-					<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
-					<a href="#!" class="b-brand">
-						->
-						
-					</a>
-					<a href="#!" class="mob-toggler">
-						<i class="feather icon-more-vertical"></i>
-					</a>
-				</div>
-				<div class="collapse navbar-collapse">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item">
-							<a href="#!" class="pop-search"><i class="feather icon-search"></i></a>
-							<div class="search-bar">
-								<input type="text" class="form-control border-0 shadow-none" placeholder="Search hear">
-								<button type="button" class="close" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-					
-						
-								<div class="dropdown-menu profile-notification ">
-									<ul class="pro-body">
-										<li><a href="/logout" class="dropdown-item"><i class="fas fa-circle"></i> Logout</a></li>
-										{{-- <li><a href="email_inbox.html" class="dropdown-item"><i class="fas fa-circle"></i> My Messages</a></li> --}}
-										<li><a href="auth-signin.html" class="dropdown-item"><i class="fas fa-circle"></i> Lock Screen</a></li>
-									</ul>
-								</div>
-							</div>
-						</li>
-						<li class="nav-item">
-							<div class="dropdown mega-menu">
-								
-								<div class="dropdown-menu profile-notification ">
-									<div class="row no-gutters">
-										<div class="col">
-										
-											
-										</div>
-										<div class="col">
-											<h6 class="mega-title">Forms</h6>
-								
-										
-									</div>
-								</div>
-							</div>
-						</li>
-					</ul>
-					<ul class="navbar-nav ml-auto">
-						<li>
-							<div class="dropdown">
-							
-								</a>
-								
-						</li>
-						<li>
-							<div class="dropdown drp-user">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-top: 20px;font-size: 44px;">
-                                    <i class="feather icon-user"></i>
-                                </a>
-                                
-								<div class="dropdown-menu dropdown-menu-right profile-notification">
-									<div class="pro-head">
-										{{-- <img src="" class="" alt="User-Profile-Image"> --}}
-										<span>PublishingHelp</span>
-										<a href="auth-signin.html" class="dud-logout" title="Logout">
-											<i class="feather icon-log-out"></i>
-										</a>
-									</div>
-									<ul class="pro-body">
-										<li><a href="{{ route('logout') }}" class="dropdown-item"><i class="feather icon-log-out"></i>
-											Logout</a></li>
-
-									</ul>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</div>
+		<div class="m-header">
+			<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
+			<a href="#!" class="b-brand">
+				<!-- ========   change your logo hear   ============ -->
 				
+			</a>
+			<a href="#!" class="mob-toggler">
+				<i class="feather icon-more-vertical"></i>
+			</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a href="#!" class="pop-search"><i class="feather icon-search"></i></a>
+					<div class="search-bar">
+						<input type="text" class="form-control border-0 shadow-none" placeholder="Search hear">
+						<button type="button" class="close" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				</li>
+				<li class="nav-item">
+				
+				
+					</div>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li>
+					<div class="dropdown">
+					
+						</a>
+						
+				</li>
+				<li>
+					<div class="dropdown drp-user">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-top: 20px;font-size: 44px;">
+							<i class="feather icon-user"></i>
+						</a>
+						
+						<div class="dropdown-menu dropdown-menu-right profile-notification">
+							<div class="pro-head">
+								{{-- <img src="" class="" alt="User-Profile-Image"> --}}
+								<span>PublishingHelp</span>
+								<a href="auth-signin.html" class="dud-logout" title="Logout">
+									<i class="feather icon-log-out"></i>
+								</a>
+							</div>
+							<ul class="pro-body">
+								<li><a href="{{ route('logout') }}" class="dropdown-item"><i class="feather icon-log-out"></i>
+									Logout</a></li>
+
+								
+							</ul>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
 			
 	</header>
 		<nav class="pcoded-navbar  ">
 		<div class="navbar-wrapper  ">
 			<div class="navbar-content scroll-div " >
-				
-				
-					
-				
 				<ul class="nav pcoded-inner-navbar ">
 					<li class="nav-item pcoded-menu-caption">
 						<label>Navigation</label>
