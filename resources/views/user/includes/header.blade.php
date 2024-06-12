88f0fc9852a6c55fa018a4fff5ae8d5f649886f9
Arpit Parihar <parihararpiy@gmail.com>
2024-06-12 17:06:33 +0530

    initial commit

--- resources/views/user/includes/header.blade.php
+++ resources/views/user/includes/header.blade.php
@@ -61,6 +61,16 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 <style>
+
+@media only screen and (max-width: 768px) {
+    .service-container {
+        max-height: none; /* Remove max-height for smaller screens */
+        height: auto; /* Allow the container to expand based on content */
+    }
+}
+
+
+
     .service-container {
         background-color: white;
         border-bottom: 8px solid maroon;
